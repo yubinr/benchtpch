233e5913324849740cfcbca312ecfdfb13a96d3a
红颜 <diane.xhy@alibaba-inc.com>
2023-10-18 20:12:32 +0800

    add new columnar.ddl

--- dbgen-src/tpcd.h
+++ dbgen-src/tpcd.h
@@ -112,6 +112,15 @@
 #define SET_DBASE       ".LOGON %s\n"
 #endif
 
+#ifdef MYSQL
+#define GEN_QUERY_PLAN "EXPLAIN PLAN"
+#define START_TRAN "START TRANSACTION"
+#define END_TRAN "COMMIT"
+#define SET_OUTPUT ""
+#define SET_ROWCOUNT "limit %d;\n"
+#define SET_DBASE "use %s;\n"
+#endif
+
 #define MAX_VARS      8 /* max number of host vars in any query */
 #define QLEN_MAX   2048 /* max length of any query */
 #define QUERIES_PER_SET 22
